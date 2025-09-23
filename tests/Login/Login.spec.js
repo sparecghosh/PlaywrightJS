e3f9843e36116feb11662d3fbd95a131d7ea321b
sparecghosh <sparecghosh@gmail.com>
2025-09-23 17:14:55 +0100

    Commented code

--- tests/Login/Login.spec.js
+++ tests/Login/Login.spec.js
@@ -1,20 +1,20 @@
-import { test, expect } from '@playwright/test';
-import PomManager from '../../Utilities/PomManager';
+// import { test, expect } from '@playwright/test';
+// import PomManager from '../../Utilities/PomManager';
 
-let pm;
+// let pm;
 
-test.describe('Login Tests', () => {
+// test.describe('Login Tests', () => {
 
-    test.beforeEach(async ({ page }) => {
-        pm = new PomManager(page);
-    });
+//     test.beforeEach(async ({ page }) => {
+//         pm = new PomManager(page);
+//     });
 
-    test('Successful Login @smoke', async () => {
-    await pm.NavigationHelper.gotoLoginPage();
-    await pm.LoginPageMethods.login('standard_user', 'secret_sauce');
-    await pm.LoginPageMethods.Moonch();
-    await pm.HomePageMethods.verifyHomePageLoaded();
-    });
+//     test('Successful Login @smoke', async () => {
+//     await pm.NavigationHelper.gotoLoginPage();
+//     await pm.LoginPageMethods.login('standard_user', 'secret_sauce');
+//     await pm.LoginPageMethods.Moonch();
+//     await pm.HomePageMethods.verifyHomePageLoaded();
+//     });
 
-});
+// });
 
